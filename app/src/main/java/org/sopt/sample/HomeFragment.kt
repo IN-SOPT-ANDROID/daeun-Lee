90f1b91082f808bec474497290a458b7fdcaa48b
Daeun <eldms0406@naver.com>
2022-12-24 01:42:42 +0900

    [add] Timber 사용해보기

--- app/src/main/java/org/sopt/sample/HomeFragment.kt
+++ app/src/main/java/org/sopt/sample/HomeFragment.kt
@@ -1,28 +1,29 @@
 package org.sopt.sample
 
-import org.sopt.sample.adapter.rvAdapter
-import org.sopt.sample.databinding.FragmentHomeBinding
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
+import org.sopt.sample.adapter.rvAdapter
 import org.sopt.sample.base.BindingFragment
-import org.sopt.sample.databinding.FragmentSearchBinding
-import org.sopt.sample.remote.*
+import org.sopt.sample.databinding.FragmentHomeBinding
+import org.sopt.sample.remote.ResponseUserDTO
+import org.sopt.sample.remote.UserServicePool
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import timber.log.Timber
 
-class HomeFragment: BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
-    private var _binding : FragmentHomeBinding? = null
+class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    private var _binding: FragmentHomeBinding? = null
         get() = requireNotNull(_binding)
     private val userService = UserServicePool.userService
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        if(BuildConfig.DEBUG){
+            Timber.plant(Timber.DebugTree())
+        }
         val adapter = rvAdapter(requireContext())
         binding.rvRepos.adapter = adapter
         userService.user().enqueue(object : Callback<ResponseUserDTO> {
@@ -30,9 +31,9 @@ class HomeFragment: BindingFragment<FragmentHomeBinding>(R.layout.fragment_home)
                 call: Call<ResponseUserDTO>,
                 response: Response<ResponseUserDTO>
             ) {
-                Log.d("유저 정보", "${response.body()}")
                 if (response.isSuccessful) {
                     response.body()?.let {
+                        Timber.d("${response.body()}")
                         adapter.setRepoList(it.data)
                     }
                 }
